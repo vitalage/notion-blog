29cacd77715dd8796384d1d62cbfc1f8df98c265
vitalage <78438398+vitalage@users.noreply.github.com>
2021-02-03 04:45:01 +0900

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
+<!–
       <span>Deploy your own!</span>
       <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
         <img
@@ -18,6 +19,7 @@ export default () => (
           view source
         </ExtLink>
       </span>
+      –>
     </footer>
   </>
 )
