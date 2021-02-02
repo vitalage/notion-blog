e66afbe663a97884c8dba1b49093b99c2d895053
vitalage <78438398+vitalage@users.noreply.github.com>
2021-02-03 04:18:22 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,25 +14,22 @@ const navItems: { label: string; page?: string; link?: string }[] = [
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
 
 export default ({ titlePre = '' }) => {
-@@ -30,21 +21,6 @@ export default ({ titlePre = '' }) => {
+  const { pathname } = useRouter()
+
+  return (
+    <header className={styles.header}>
+      <Head>
+        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <meta
+          name="description"
+          content="An example Next.js site using Notion for the blog"
+        />
+        <meta name="og:title" content="My Notion Blog" />
+        <meta property="og:image" content={ogImageUrl} />
+        <meta name="twitter:site" content="@_ijjk" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
           <li key={label}>
-            {page ? (
-              <Link href={page}>
-                <a className={pathname === page ? 'active' : undefined}>
-                  {label}
-                </a>
-              </Link>
-            ) : (
-              <ExtLink href={link}>{label}</ExtLink>
-            )}
-          </li>
-        ))}
-      </ul>
-    </header>
-  )
-}
